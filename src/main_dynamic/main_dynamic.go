d54402f0fecc0689e7a45e759035b804acc09ebf
CWorld <cworld0@qq.com>
2024-06-10 23:40:28 +0800

    feat(go-pingpong): :sparkles: add server

--- src/main_dynamic/main_dynamic.go
+++ src/main_dynamic/main_dynamic.go
@@ -7,11 +7,39 @@ package main
 #include "../../lib/pingpong.h"
 */
 import "C"
-import "fmt"
+import (
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	// C.pingpong(C.CString("world"))
-	var result = C.GoString(C.get_best_action(C.CString("SB_2")))
-	fmt.Println(result)
 	C.whisper(C.CString("(this is code from the dynamic library)"))
+	start_server()
+}
+
+func start_server() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/best_action", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": best_action(),
+		})
+	})
+
+	r.Run() // listen and serve on
+}
+
+func best_action() map[string]interface{} {
+	var result = C.GoString(C.get_best_action(C.CString("SB_2")))
+	// fmt.Println(result)
+	var format_result map[string]interface{}
+	json.Unmarshal([]byte(result), &format_result)
+	return format_result
 }
