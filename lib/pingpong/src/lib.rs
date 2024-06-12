197d75622a53ea9c404d0bf2b7f12de6bb424571
CWorld <cworld0@qq.com>
2024-06-12 20:39:43 +0800

    perf(rust_pingpong): :rocket: update test code

--- lib/pingpong/src/lib.rs
+++ lib/pingpong/src/lib.rs
@@ -17,7 +17,8 @@ pub extern "C" fn get_best_action(action: *const libc::c_char) -> *const libc::c
     let action_cstr = unsafe { CStr::from_ptr(action) };
     let action = action_cstr.to_str().unwrap();
 
-    let data = data::PingpongData::init("data/transition_matrix.csv").unwrap();
+    let data = data::PingpongData::init("data/transition_matrix_lite.csv").unwrap();
+
     let result = calc::suggest_best_action(&data, action).unwrap();
     // println!("{:?}", result);
 
@@ -35,21 +36,28 @@ pub mod test {
     use ndarray::Array1;
     use std::{error::Error, ffi::CString};
 
+    fn whisper_rust() {
+        whisper(CString::new("(this is code from Rust)").unwrap().into_raw());
+    }
+
     // This is meant to do the same stuff as the main function in the .go files
     #[test]
     fn simulated_main_function() {
-        whisper(get_best_action(CString::new("SF_2").unwrap().into_raw()));
-        whisper(CString::new("(this is code from Rust)").unwrap().into_raw());
+        whisper_rust();
+        println!("Best action:");
+        whisper(get_best_action(CString::new("LB_1").unwrap().into_raw()));
     }
 
     #[test]
     pub fn calc_evaluate_strategy() -> Result<(), Box<dyn Error>> {
+        whisper_rust();
+
         let data = data::PingpongData::init("data/transition_matrix.csv")?;
 
         // Example initial vector, as the initial state is S_2,
         // meaning the second player starts the game
         let mut initial_vector: Array1<f64> = Array1::zeros(data.states.len());
-        if let Some(index) = data.states.iter().position(|s| s == "S_2") {
+        if let Some(index) = data.states.iter().position(|s| s == "LB_1") {
             initial_vector[index] = 1.0;
         }
 
